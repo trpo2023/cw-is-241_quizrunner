788d2b92ed60a3fc2c9299ca21d4458092969214
Alexander Ivanov <sanekj.sanek9884@gmail.com>
2023-05-23 23:47:04 +0700

    Add from_json() method meant for deserialization
    The method reads json file, creates a new Quiz class object and assigns every class attributes to particular jsong data value
    (Quiz name, time limit e.t.c)
     # Пожалуйста, введите сообщение коммита для ваших изменений. Строки,

--- quiz.py
+++ quiz.py
@@ -19,3 +19,18 @@ class Quiz:
             questions.append(question)
         with open('quiz_parser.json', 'w') as f:
             json.dump({"quiz_name": self.quiz_name, "time_limit": self.time_limit,"question_list": questions}, f, indent=4)
+     
+    @staticmethod
+    def from_json():
+        with open('quiz_parser.json') as f:
+            data = json.load(f)
+        question_list = []
+        answer_list = []
+        correct_answer_list = []
+        quiz_name = data['quiz_name']
+        time_limit = data['time_limit']
+        for iterator in data['question_list']:
+            question_list.append(iterator['question'])
+            answer_list.append(iterator['answer_list'])
+            correct_answer_list.append(iterator['correct_answer'])
+        return Quiz(quiz_name, time_limit, question_list, answer_list, correct_answer_list)
