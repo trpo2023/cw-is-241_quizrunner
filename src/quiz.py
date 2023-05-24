eac565238358874d1128f90a95c260e445baa9ee
khubik2 <>
2023-05-24 09:02:05 +0700

    Format code

--- src/quiz.py
+++ src/quiz.py
@@ -1,7 +1,10 @@
 import json
 
+
 class Quiz:
-    def __init__(self, quiz_name, time_limit, question_list, answer_list, correct_answer_list):
+    def __init__(
+        self, quiz_name, time_limit, question_list, answer_list, correct_answer_list
+    ):
         self.quiz_name = quiz_name
         self.time_limit = time_limit
         self.question_list = question_list
@@ -14,22 +17,32 @@ class Quiz:
             question = {
                 "question": self.question_list[i],
                 "answer_list": self.answer_list[i],
-                "correct_answer": self.correct_answer_list[i]
+                "correct_answer": self.correct_answer_list[i],
             }
             questions.append(question)
-        with open('quiz_parser.json', 'w') as f:
-            json.dump({"quiz_name": self.quiz_name, "time_limit": self.time_limit,"question_list": questions}, f, indent=4)
-     
+        with open("quiz_parser.json", "w") as f:
+            json.dump(
+                {
+                    "quiz_name": self.quiz_name,
+                    "time_limit": self.time_limit,
+                    "question_list": questions,
+                },
+                f,
+                indent=4,
+            )
+
     @staticmethod
     def from_json(f):
         data = json.load(f)
         question_list = []
         answer_list = []
         correct_answer_list = []
-        quiz_name = data['quiz_name']
-        time_limit = data['time_limit']
-        for iterator in data['question_list']:
-            question_list.append(iterator['question'])
-            answer_list.append(iterator['answer_list'])
-            correct_answer_list.append(iterator['correct_answer'])
-        return Quiz(quiz_name, time_limit, question_list, answer_list, correct_answer_list)
+        quiz_name = data["quiz_name"]
+        time_limit = data["time_limit"]
+        for iterator in data["question_list"]:
+            question_list.append(iterator["question"])
+            answer_list.append(iterator["answer_list"])
+            correct_answer_list.append(iterator["correct_answer"])
+        return Quiz(
+            quiz_name, time_limit, question_list, answer_list, correct_answer_list
+        )
--- src/testpreview.py
+++ src/testpreview.py
@@ -2,46 +2,46 @@ import tkinter as tk
 import tkinter.ttk as ttk
 from src.testrunner import TestRunner
 
+
 class TestPreview:
     def __init__(self, quiz):
-       # build ui
+        # build ui
         qcount = len(quiz.question_list)
         time = quiz.time_limit
         testname = quiz.quiz_name
-        self.savedquiz=quiz
+        self.savedquiz = quiz
         self.TestPreview = tk.Toplevel()
         self.TestPreview.configure(height=200, width=200)
         labelframe3 = ttk.Labelframe(self.TestPreview)
-        labelframe3.configure(
-            height=200,
-            text='Run this test?',
-            width=500)
+        labelframe3.configure(height=200, text="Run this test?", width=500)
         label5 = ttk.Label(labelframe3)
-        label5.configure(justify="left", text='Number of questions:')
+        label5.configure(justify="left", text="Number of questions:")
         label5.grid(column=0, row=1, sticky="e")
         self.pre_numqs = ttk.Label(labelframe3)
-        self.pre_numqs.configure(text=' '+str(qcount))
+        self.pre_numqs.configure(text=" " + str(qcount))
         self.pre_numqs.grid(column=1, row=1, sticky="w")
         label8 = ttk.Label(labelframe3)
-        label8.configure(text='Time provided:')
+        label8.configure(text="Time provided:")
         label8.grid(column=0, row=2, sticky="e")
         label9 = ttk.Label(labelframe3)
         label9.grid(column=1, row=2)
         self.pre_time = ttk.Label(labelframe3)
-        self.pre_time.configure(text=' %d:%d' % (time/60, time%60))
+        self.pre_time.configure(text=" %d:%d" % (time / 60, time % 60))
         self.pre_time.grid(column=1, row=2, sticky="w")
         label14 = ttk.Label(labelframe3)
-        label14.configure(text='Avg. per question:')
+        label14.configure(text="Avg. per question:")
         label14.grid(column=0, row=3, sticky="e")
         self.pre_perq = ttk.Label(labelframe3)
-        self.pre_perq.configure(text=' %d:%d' % (time/qcount/60, time/qcount%60))
+        self.pre_perq.configure(
+            text=" %d:%d" % (time / qcount / 60, time / qcount % 60)
+        )
         self.pre_perq.grid(column=1, row=3, sticky="w")
         self.pre_cancel = ttk.Button(labelframe3)
-        self.pre_cancel.configure(text='Cancel', width=16)
+        self.pre_cancel.configure(text="Cancel", width=16)
         self.pre_cancel.grid(column=0, columnspan=5, pady=4, row=6)
         self.pre_cancel.configure(command=self.pwcancel)
         self.pre_run = ttk.Button(labelframe3)
-        self.pre_run.configure(text='Run', width=16)
+        self.pre_run.configure(text="Run", width=16)
         self.pre_run.grid(column=0, columnspan=2, row=5)
         self.pre_run.configure(command=self.pwok)
         self.pre_name = ttk.Label(labelframe3)
